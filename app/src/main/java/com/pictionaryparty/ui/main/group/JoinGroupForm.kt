b7ac77ffaca3f0294937262556209635f2bb3208
francesca345 <127933947+francesca345@users.noreply.github.com>
2024-04-20 09:52:20 +0200

    change font

--- app/src/main/java/com/pictionaryparty/ui/main/group/JoinGroupForm.kt
+++ app/src/main/java/com/pictionaryparty/ui/main/group/JoinGroupForm.kt
@@ -2,4 +2,5 @@ package com.pictionaryparty.ui.main.group
 
 fun JoinGroupForm() {
     // Add code to join a group
+    // chiede nome gruppo e codice
 }
--- app/src/main/java/com/pictionaryparty/ui/theme/Color.kt
+++ app/src/main/java/com/pictionaryparty/ui/theme/Color.kt
@@ -1,6 +1,7 @@
 package com.pictionaryparty.ui.theme
 
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
 
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
@@ -10,8 +11,8 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
-val PrimaryButtonColor: Color = Color(0xFF6200EE)
-val SecondaryButtonColor = Color(0xFF03DAC5)
+val PrimaryButtonColor: Color = Color(39,68,92)
+val SecondaryButtonColor = Color(44,53,60)
 val DefaultButtonColor = Color(0xFF6200EE)
-val LightTextColor = Color(0xFFFFFFFF)
+val LightTextColor = Color(191,215,237)
 val DefaultTextColor = Color(0xFF6200EE)
