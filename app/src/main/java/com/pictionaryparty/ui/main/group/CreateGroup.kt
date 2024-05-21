c5e285ce58a2ece0166f88175b936a8e7eb239a2
arloghiro <119437097+arloghiro@users.noreply.github.com>
2024-05-21 18:36:00 +0200

    inizio del reset delle variabili

--- app/src/main/java/com/pictionaryparty/ui/main/group/CreateGroup.kt
+++ app/src/main/java/com/pictionaryparty/ui/main/group/CreateGroup.kt
@@ -1,10 +1,13 @@
 package com.pictionaryparty.ui.main.group
 
+import androidx.activity.compose.BackHandler
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import com.pictionaryparty.data.GameConnectionState
 import com.pictionaryparty.ui.main.MainViewModel
 
@@ -14,6 +17,11 @@ fun CreateGroup(viewModel: MainViewModel) {
     val gameStatus by viewModel.gameConnectionState.collectAsState()
     val channel by viewModel.connectedChannel.collectAsState(initial = null)
     val bottomSheetScaffoldState = rememberBottomSheetScaffoldState()
+
+    BackHandler {
+        viewModel.resetVariables()
+    }
+
     GroupBottomSheet(
         bottomSheetScaffoldState = bottomSheetScaffoldState,
         expand = gameStatus is GameConnectionState.Success,
